bf7699c9435004dc7210aefaa5ac5abe41eece44
jhamile08 <jhamiledr222@gmail.com>
2024-11-15 06:29:48 -0500

    roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :events
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: { user: 'user', moderator: 'moderator', admin: 'admin' }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
